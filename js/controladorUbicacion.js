21cd9699cf6b7814c8adf9ba2948ebc0681a0658
Franklin-Amador <joelamdor55@gmail.com>
2022-08-14 22:03:06 -0600

    cambios de estado en ordenes

--- js/controladorUbicacion.js
+++ js/controladorUbicacion.js
@@ -47,3 +47,88 @@ function cambiarEstado(){
     location.href="../html/ubicacion.html"
     
 }
+
+function enOrigen(){
+    axios ( {
+        url :'http://localhost:3000/motoristas/historialOrden/pedido',
+        method : 'get',
+        responseType : 'json'
+    } ).then( res => {
+
+        let x = res.data;
+        console.log(res.data)
+        document.getElementById('estado').innerHTML = '';
+
+        for ( let i = 0 ; i < x.length ; i++ ) {
+            
+            for(let j = 0 ; j < x[i].pedidos.length ; j++){
+                if(x[i].pedidos[j].estado == 'Pendiente'){
+                    document.getElementById('estado').innerHTML += `              
+                            <h3>Estado: ${x[i].pedidos[j].estado = 'en Origen'}</h3>
+                
+                    `  
+                }
+            }
+        }   
+
+    }).catch( err => {
+        console.log(err);
+    })
+}
+
+function enCamino(){
+    axios ( {
+        url :'http://localhost:3000/motoristas/historialOrden/pedido',
+        method : 'get',
+        responseType : 'json'
+    } ).then( res => {
+
+        let x = res.data;
+        console.log(res.data)
+        document.getElementById('estado').innerHTML = '';
+
+        for ( let i = 0 ; i < x.length ; i++ ) {
+            
+            for(let j = 0 ; j < x[i].pedidos.length ; j++){
+                if(x[i].pedidos[j].estado == 'en Origen'){
+                    document.getElementById('estado').innerHTML += `              
+                            <h3>Estado: ${x[i].pedidos[j].estado = 'en Camino'}</h3>
+                
+                    `  
+                }
+            }
+        }   
+
+    }).catch( err => {
+        console.log(err);
+    })
+}
+
+function enDestino(){
+    axios ( {
+        url :'http://localhost:3000/motoristas/historialOrden/pedido',
+        method : 'get',
+        responseType : 'json'
+    } ).then( res => {
+
+        let x = res.data;
+        console.log(res.data)
+        document.getElementById('estado').innerHTML = '';
+
+        for ( let i = 0 ; i < x.length ; i++ ) {
+            
+            for(let j = 0 ; j < x[i].pedidos.length ; j++){
+                if(x[i].pedidos[j].estado == 'en Camino'){
+                    document.getElementById('estado').innerHTML += `              
+                            <h3>Estado: ${x[i].pedidos[j].estado = 'en Destino'}</h3>
+                
+                    `  
+                }
+            }
+        }   
+
+    }).catch( err => {
+        console.log(err);
+    })
+}
+
