5d989d4f4f10d9577485578736385bb05138b236
Domaa2022 <victormiguelbarahonalicona@gmail.com>
2022-08-17 14:32:49 -0600

    actualizacion final

--- js/controladorOrdenesSolicitada.js
+++ js/controladorOrdenesSolicitada.js
@@ -47,3 +47,27 @@ function generarProductos (){
 }
 
 generarProductos();
+
+
+function display(a){
+    switch(a){
+        case 1:
+        location.href = "../html/disponibles.html"
+        break;
+        case 2:
+            location.href = "../html/ordenes.html"
+            break;
+        case 3: 
+            location.href = "../html/detallesTomada.html"
+            break;
+        case 4:
+            location.href ="../html/inicio.html"
+            break;
+        case 5:
+            location.href ="../html/detallesOrden.html"
+            break;
+        case 6:
+            location.href="../html/detalles.html"
+            break;
+    }
+}
